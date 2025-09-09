717a0891cbb3a027772ef6dd5ae36d5f4db25119
Paria_ <paria.heidari@knowit.no>
2025-09-09 14:10:29 +0200

    change sdk to google/genai

--- app/api/generate-gemini-ai/route.ts
+++ app/api/generate-gemini-ai/route.ts
@@ -1,24 +1,25 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
 import { NextResponse } from "next/server";
+import { GoogleGenAI } from "@google/genai";
 
 const GOOGLE_GENAI_API_KEY = process.env.GEMINI_API_KEY as string;
 // or "gemini-1.5-pro"
 const MODEL = "gemini-2.0-flash";
 
 export async function POST (req: Request): Promise<Response> {
-
   try{
     const data = await req.json();
     const prompt = data.prompt;
     if(!prompt){
       return NextResponse.json({error: "Missing prompt"}, {status:400});
     }
-
-    const generativeAI = new GoogleGenerativeAI(GOOGLE_GENAI_API_KEY);
-    const model = generativeAI.getGenerativeModel({model: MODEL});
-    const result = await model.generateContent(prompt);
-    const summary = await result.response.text();
     
+    const generativeAI = new GoogleGenAI({ apiKey: GOOGLE_GENAI_API_KEY });
+    const result = await generativeAI.models.generateContent({
+      model: MODEL,
+      contents: prompt
+    });
+    const summary = result.text;
+
     return NextResponse.json({summary});
 
   }catch (err: any){
@@ -26,5 +27,4 @@ export async function POST (req: Request): Promise<Response> {
     return NextResponse.json({error: err?.message ?? "Server Error"}, {status:500});
   }
 }
-
 
