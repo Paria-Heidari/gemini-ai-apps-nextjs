d66a453e484c7540e90a786130d5878753b9175a
Paria_ <paria.heidari@knowit.no>
2025-09-09 14:12:24 +0200

    implement FE & fix styles

---
+++ app/components/PromptForm.tsx
@@ -0,0 +1,80 @@
+"use client";
+import { FormEvent, useState } from "react";
+import styles from './PromptForm.module.css';
+
+const baseUrl = "http://localhost:3000";
+const promptInit = "Gemini vs ChatGPT";
+
+const PromptForm = () => {
+  const [loading, setLoding] = useState(false);
+  const [summary, setSummary] = useState<string|null>(null);
+  const [prompt, setPrompt] = useState<string>(promptInit);
+
+
+  const generateText = async (event: FormEvent) => {
+    event.preventDefault();
+    setSummary(null);
+
+    setLoding(true);
+    try {
+      const response = await fetch(`${baseUrl}/api//generate-gemini-ai`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ prompt }),
+      });
+
+      const data = await response.json();
+
+      if (!response.ok){
+        console.error(data?.error);
+
+      }else {
+        setSummary(data.summary);
+      }
+
+    } catch (error) {
+      console.error("Error:", error);
+      return null;
+    }
+    setLoding(false);
+  };
+  return (
+    <>
+      <form onSubmit={generateText} className={styles.form}>
+        <label className={styles.label}>Prompt</label>
+        <textarea
+          value={prompt}
+          rows={5}
+          onChange={(e) => setPrompt(e.target.value)}
+          className={styles.textarea}
+          required
+        />
+        <div className={styles.actions}>
+          <button type="submit" className={styles.button} disabled={loading}>
+            {loading ? "Generating..." : "Generate"}
+          </button>
+          <button
+            type="button"
+            className={styles.button}
+            onClick={() => {
+              setPrompt("");
+              setSummary(null);
+            }}
+          >
+            Clear
+          </button>
+        </div>
+      </form>
+      {summary && (
+        <div className={styles.summaryCard}>
+          <h2> Response </h2>
+          <div className={styles.summary}>{summary}</div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default PromptForm;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import PromptForm from "./components/PromptForm";
+
 
 export default function Home() {
+
   return (
-    <div className={styles.page}>
-     <h1>Test</h1>
-    </div>
+    <main>
+      <div className={styles.page}>
+        <PromptForm/>
+      </div>
+    </main>
   );
 }
